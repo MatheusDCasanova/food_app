e8cbd407b4d921d06974fd0f6e2f4646455d1dad
Matheus Domingues Casanova Nogueira <matheuscasanova@Matheuss-MacBook-Pro.local>
2023-02-21 23:32:40 +0000

    theme and styles aplied

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         canvasColor: const Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-              headline6: const TextStyle(
+              titleLarge: const TextStyle(
                 fontSize: 20,
                 fontFamily: 'RobotoCondensed',
               ),
