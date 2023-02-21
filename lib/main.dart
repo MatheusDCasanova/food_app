2ffbef8c6dad3fdf90b13c1bcb7af6685c790100
Matheus Domingues Casanova Nogueira <matheuscasanova@Matheuss-MacBook-Pro.local>
2023-02-21 22:14:38 +0000

    theme and styles aplied

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:food_app/screens/categories_screen.dart';
+import 'screens/categories_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'Vamos Cozinhar?',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.pink,
+          secondary: Colors.amber,
+        ),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
-      home: CategoriesScreen(),
+      home: const CategoriesScreen(),
     );
   }
 }
