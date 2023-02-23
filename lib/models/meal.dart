fc868a72136b0a3ef17720864dda51f8eaa60e49
Matheus Domingues Casanova Nogueira <matheuscasanova@Matheuss-MacBook-Pro.local>
2023-02-23 16:59:59 +0000

    add meal model

---
+++ lib/models/meal.dart
@@ -0,0 +1,43 @@
+enum Complexity {
+  Simple,
+  Medium,
+  Difficult,
+}
+
+enum Cost {
+  Cheap,
+  Fair,
+  Expensive,
+}
+
+class Meal {
+  final String id;
+  final String title;
+  final List<String> categories;
+  final List<String> ingredients;
+  final List<String> steps;
+  final String imageUrl;
+  final int duration;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
+  final Complexity complexity;
+  final Cost cost;
+
+  const Meal({
+    required this.id,
+    required this.title,
+    required this.categories,
+    required this.imageUrl,
+    required this.duration,
+    required this.steps,
+    required this.ingredients,
+    required this.cost,
+    required this.complexity,
+    this.isGlutenFree = false,
+    this.isVegan = false,
+    this.isLactoseFree = false,
+    this.isVegetarian = false,
+  });
+}
