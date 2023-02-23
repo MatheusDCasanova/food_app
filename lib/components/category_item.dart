2b86946f98e5a35eb62cc11c04ff5c17ca377baa
Matheus Domingues Casanova Nogueira <matheuscasanova@Matheuss-MacBook-Pro.local>
2023-02-23 16:32:17 +0000

    routes assigned to constant variables

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/utils/app_routes.dart';
 import '../models/category.dart';
 import '../screens/categories_meals_screen.dart';
 
@@ -11,7 +12,7 @@ class CategoryItem extends StatelessWidget {
 
   void _selectCategory(BuildContext context) {
     Navigator.of(context).pushNamed(
-      '/categories-meals',
+      AppRoutes.CATEGORIES_MEALS,
       arguments: category,
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
+import 'utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,8 +29,8 @@ class MyApp extends StatelessWidget {
             ),
       ),
       routes: {
-        '/': (context) => const CategoriesScreen(),
-        '/categories-meals': (context) => CategoriesMealsScreen(),
+        AppRoutes.HOME: (context) => const CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (context) => CategoriesMealsScreen(),
       },
     );
   }
